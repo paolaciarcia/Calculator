5e1fff8892afb1aea03081a5016524cfc6234ff2
Paola Ciarcia <paolaciarcia20@gmail.com>
2021-06-15 17:54:41 -0300

    add functionality to calculator

--- calculator/ViewController.swift
+++ calculator/ViewController.swift
@@ -8,21 +8,52 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var displayLabel: UILabel!
     
+    private var isFinishedTypingNumber: Bool = true
+    
     @IBAction func calcButtonPressed(_ sender: UIButton) {
+        isFinishedTypingNumber = true
+        
+        guard let number = Double(displayLabel.text!) else { return }
+        
+        switch sender.currentTitle {
+        case "AC":
+            displayLabel.text = "0"
+        case "+/-":
+            displayLabel.text = String(number * -1)
+        case "%":
+            displayLabel.text = String(number / 100)
+        default:
+            return
+        }
     }
     
     
     @IBAction func numButtonPressed(_ sender: UIButton) {
+        guard let textLabel = sender.currentTitle, let label = Double(displayLabel.text!) else { return }
+        
+        if isFinishedTypingNumber == true {
+            displayLabel.text = textLabel
+            isFinishedTypingNumber = false
+        } else {
+            
+            if textLabel == "." {
+                let isInt = floor(label) == label
+                if !isInt {
+                    return
+                }
+            }
+            displayLabel.text = displayLabel.text! + textLabel
+        }
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    
 }
 
