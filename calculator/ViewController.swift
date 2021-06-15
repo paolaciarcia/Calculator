a9a94597d231faa6ebe53b315fd1ac1ad2faf9b6
Paola Ciarcia <paolaciarcia20@gmail.com>
2021-06-15 15:00:49 -0300

    link IBOutlets and IBActions from Storyboard to ViewController

--- calculator/ViewController.swift
+++ calculator/ViewController.swift
@@ -9,6 +9,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var displayLabel: UILabel!
+    
+    @IBAction func calcButtonPressed(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func numButtonPressed(_ sender: UIButton) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
